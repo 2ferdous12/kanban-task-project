3a2e56c402fc8b10abd10fed647f8bd1682a4edc
Ferdous020 <sheikhferdous75@gmail.com>
2024-05-05 02:55:48 -0700

    added all file and style

--- src/homesection/mainhome.jsx
+++ src/homesection/mainhome.jsx
@@ -1,11 +1,17 @@
-import { FaEye } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+
 
 const mainhome = () => {
+  const [showEmptyBoard, setShowEmptyBoard] = useState(true);
 
+  const toggleSections = () => {
+    setShowEmptyBoard(!showEmptyBoard);
+  };
 return (
 <div className="min-h-[calc(100vh-90px)] w-full bg-[#F4F7FD] ">
+<div className="min-h-[calc(100vh-90px)] w-full bg-[#F4F7FD]">
+      {showEmptyBoard ? (
+
 <div className=" bg-[#F4F7FD] 
  w-full flex justify-center items-center min-h-[calc(100vh-120px)] ">
  <div className="text-center space-y-3">
@@ -13,23 +19,22 @@ return (
  This board is empty. Create a new column to get started.
  </h1>
  {/* to="/platforms" */}
- <Link to="/columnSection"
+ <div onClick={toggleSections}
  className={`btn text-white border-none
   h-[50px] bg-[#635FC7] font-semibold text-xl
    hover:bg-[#A8A4FF] rounded-full`}>+ Add New Column
-</Link>
+</div>
  </div>
 </div>
-  <NavLink className={({isActive})=> isActive ? 'hidden'
-   : 'hidden'} >
-<div className=" rounded-r-full 
-bg-[#635FC7] flex justify-center items-center text-white
- -mt-[50px] w-[50px] h-[40px]">
-<FaEye 
- className="w-6"></FaEye>
-</div>
-</NavLink>
-
+) : (
+        <div className="bg-[#F4F7FD] w-full flex justify-center items-center min-h-[calc(100vh-120px)]">
+          {/* Content for the "Add Column" section goes here */}
+          <div>
+          <h1>hi</h1>
+          </div>
+        </div>
+      )}
+    </div>
 </div>
 
     );
